84851d47ad84611ac596d472c38fdd5d352660cc
Trevor Scott <tscott@salesforce.com>
2018-11-14 15:43:12 -0800

    fix API config var bug

--- src/api-config.js
+++ src/api-config.js
@@ -1,4 +1,4 @@
-const API_ROOT = process.env.REACT_APP_EDM_STATS_BACKEND_HOST || 'http://localhost:5001';
+const API_ROOT = process.env.REACT_APP_EDM_STREAM_BACKEND_HOST || 'http://localhost:5001';
 const API_ROOT_STATS = process.env.REACT_APP_EDM_STATS_BACKEND_HOST || 'http://localhost:5002';
 
 export {
