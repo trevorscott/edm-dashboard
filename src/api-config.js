72a7c9af01733b44a2a3ba50a9b869ae68c6a99e
Trevor Scott <tscott@salesforce.com>
2018-11-30 14:10:21 -0800

    switch to runtime env vars to address hardcoded env vars in slugs used for terraform script

--- src/api-config.js
+++ src/api-config.js
@@ -1,2 +1,4 @@
-export const API_ROOT = process.env.REACT_APP_EDM_STREAM_BACKEND_HOST || 'http://localhost:5001';
-export const API_ROOT_STATS = process.env.REACT_APP_EDM_STATS_BACKEND_HOST || 'http://localhost:5002';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+const env = runtimeEnv();
+export const API_ROOT = env.REACT_APP_EDM_STREAM_BACKEND_HOST || 'http://localhost:5001';
+export const API_ROOT_STATS = env.REACT_APP_EDM_STATS_BACKEND_HOST || 'http://localhost:5002';
