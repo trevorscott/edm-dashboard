54ee61823059d15e266cd419d9dea533f4a9e748
--global <tscott@heroku.com>
2018-12-04 13:42:09 -0800

    add nav bar with links to heroku devcenter and git repo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a className="link" href="https://devcenter.heroku.com/articles/event-driven-microservices-with-apache-kafka" target="_blank">Heroku Dev Center Article</a>
+              </li>
+              <li>
+                <a className="link" href="https://github.com/heroku-examples/edm-terraform" target="_blank">Github</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
         <Dashboard/>
       </div>
     );
